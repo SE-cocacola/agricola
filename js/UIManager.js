2617fa4bdbdf1c8982699ec894d03c486e717747
Kyoungwoong <kyy980708@gmail.com>
2023-05-03 13:21:49 +0900

    rulebook 팝업창 띄우기

--- js/UIManager.js
+++ js/UIManager.js
@@ -4,6 +4,7 @@ export class UIManager extends UIInterface {
     constructor() {
       super();
       // add any additional initialization code here
+      this.popup = null;
     }
     
     initUI() {
@@ -32,4 +33,39 @@ export class UIManager extends UIInterface {
     //       div.classList.remove("hover-red");
     //     });
     // }
+
+    // Function to create and show the popup window
+    openPopup() {
+        if (!this.popup) {
+          this.popup = document.createElement("div");
+          this.popup.classList.add("popup");
+          const popupContent = document.createElement("div");
+          popupContent.classList.add("popup-content");
+          popupContent.innerHTML = "<p>This is the popup content.</p>";
+          const closeButton = document.createElement("button");
+          closeButton.innerHTML = "Close";
+          closeButton.addEventListener("click", () => {
+            this.closePopup();
+          });
+          this.popup.appendChild(popupContent);
+          this.popup.appendChild(closeButton);
+          document.body.appendChild(this.popup);
+        }
+      }
+    
+      closePopup() {
+        if (this.popup) {
+          document.body.removeChild(this.popup);
+          this.popup = null;
+        }
+      }
+    
+      addButtonEventListeners() {
+        const buttons = document.querySelectorAll(".icon");
+        buttons.forEach((button) => {
+          button.addEventListener("click", () => {
+            this.openPopup();
+          });
+        });
+      }
 }
