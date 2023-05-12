8655b1303862b0ba86fc285734a6a9aad59df5d2
Junbal <96059603+Junbal@users.noreply.github.com>
2023-05-12 14:27:15 +0900

    Make Board Interface methods

--- js/Board/ActionSpace.js
+++ js/Board/ActionSpace.js
@@ -2,6 +2,28 @@ import BoardInterface from './BoardInterface.js'
 
 class ActionSpace extends BoardInterface {
   constructor() {}
+
+  expandFarm() {}
+
+  accumulateFood() {}
+
+  grainSeed() {}
+
+  farmland() {}
+
+  lessons() {}
+
+  dayLaborer() {}
+
+  fencing() {}
+
+  forrest() {}
+
+  clayPit() {}
+
+  reedBank() {}
+
+  fishing() {}
 }
 
 export default ActionSpace
--- js/Board/RoundSpace.js
+++ js/Board/RoundSpace.js
@@ -2,6 +2,22 @@ import BoardInterface from './BoardInterface.js'
 
 class RoundSpace extends BoardInterface {
   constructor() {}
+
+  buildMajorFacility() {}
+
+  buildFence() {}
+
+  sowSeeds() {}
+
+  bakeBread() {}
+
+  accumulateSheep() {}
+
+  increaseFamily() {}
+
+  upgradeHouse() {}
+
+  accumulateStone() {}
 }
 
 export default RoundSpace
