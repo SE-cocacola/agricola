a24255f6655a8a1a4fa1a0bfb891022c4b0db760
ohhyungsuh <ohgudtj_3@naver.com>
2023-05-23 11:35:45 +0900

    자원 매니저 점수 계산 완료

--- js/Player.js
+++ js/Player.js
@@ -16,83 +16,134 @@ class Player {
     }
 
     calculateScore() {
-        const resources = this.resourceManager.resources;
-        const tiles = this.tileManager.playerBoard;
-        let playrScore = 0;
+      const resources = this.resourceManager.resources;
+      const tiles = this.tileManager.playerBoard;
+      let playrScore = 0;
 
-        for(let i=0; i<resources.length; i++){
-          if(resources[i].resourceType === RT.GRAIN){
-            if(resources[i].amount === 0){
-              playrScore -= 1;
-            }else if(1 <= resources[i].amount && resources[i].amount <= 3){
+      // GRAIN
+      if(resources[4].amount === 0){
+        playrScore -= 1;
+      }else if(1 <= resources[4].amount && resources[4].amount <= 3){
+        playrScore += 1;
+      }else if(4 <= resources[4].amount && resources[4].amount <= 5){
+        playrScore += 2;
+      }else if(6 <= resources[4].amount && resources[4].amount <= 7){
+        playrScore += 3;
+      }else{
+        playrScore += 4;
+      }
+
+      // VEGETABLE
+      if(resources[5].amount === 0){
+        playrScore -= 1;
+      }else if(1 === resources[5].amount){
+        playrScore += 1;
+      }else if(2 === resources[5].amount){
+        playrScore += 2;
+      }else if(3 === resources[5].amount){
+        playrScore += 3;
+      }else{
+        playrScore += 4;
+      }
+      
+      // SHEEP
+      if(resources[7].amount === 0){
+        playrScore -= 1;
+      }else if(1 <= resources[7].amount && resources[7].amount <= 3){
+        playrScore += 1;
+      }else if(4 <= resources[7].amount && resources[7].amount <= 5){
+        playrScore += 2;
+      }else if(6 <= resources[7].amount && resources[7].amount <= 7){
+        playrScore += 3;
+      }else{
+        playrScore += 4;
+      }
+
+      // BOAR
+      if(resources[8].amount === 0){
+        playrScore -= 1;
+      }else if(1 <= resources[8].amount && resources[8].amount <= 2){
+        playrScore += 1;
+      }else if(3 <= resources[8].amount && resources[8].amount <= 4){
+        playrScore += 2;
+      }else if(5 <= resources[8].amount && resources[8].amount <= 6){
+        playrScore += 3;
+      }else{
+        playrScore += 4;
+      }
+
+      // CATTLE
+      if(resources[9].amount === 0){
+        playrScore -= 1;
+      }else if(1 === resources[9].amount){
+        playrScore += 1;
+      }else if(2 <= resources[9].amount && resources[9].amount <= 3){
+        playrScore += 2;
+      }else if(4 <= resources[9].amount && resources[9].amount <= 5){
+        playrScore += 3;
+      }else{
+        playrScore += 4;
+      }
+
+      // FARMER
+      playrScore += resources[14].amount * 3;
+      playrScore += resources[15].amount * 3;
+
+      // BEGGING
+      playrScore -= resources[12].amount * 3;
+      
+      // majorCard 확인, 인덱스 확인해서 점수 계산, 추가점수까지
+      for(let i=0; i<resources[10].length; i++){
+        switch(resources[10].get(i).cardIdx){
+          case 0:
+            playrScore += 1;
+          case 1:
+            playrScore += 1;
+          case 2:
+            playrScore += 1;
+          case 3:
+            playrScore += 1;
+          case 4:
+            playrScore += 2;
+          case 5:
+            playrScore += 3;
+          case 6: // 가구
+            playrScore += 2;
+            if(3 <= resources[0].amount && resources[0].amount < 5){
               playrScore += 1;
-            }else if(4 <= resources[i].amount && resources[i].amount <= 5){
+            }else if(5 <= resources[0].amount && resources[0].amount < 7){
               playrScore += 2;
-            }else if(6 <= resources[i].amount && resources[i].amount <= 7){
+            }else if(7 <= resources[0].amount){
               playrScore += 3;
-            }else{
-              playrScore += 4;
             }
-          }else if(resources[i].resourceType === RT.VEGETABLE){
-            if(resources[i].amount === 0){
-              playrScore -= 1;
-            }else if(1 === resources[i].amount){
+          case 7: // 그릇
+            playrScore += 2;
+            if(3 <= resources[1].amount && resources[1].amount < 5){
               playrScore += 1;
-            }else if(2 === resources[i].amount){
+            }else if(5 <= resources[1].amount && resources[1].amount < 7){
               playrScore += 2;
-            }else if(3 === resources[i].amount){
+            }else if(7 <= resources[1].amount){
               playrScore += 3;
-            }else{
-              playrScore += 4;
             }
-          }else if(resources[i].resourceType === RT.SHEEP){
-            if(resources[i].amount === 0){
-              playrScore -= 1;
-            }else if(1 <= resources[i].amount && resources[i].amount <= 3){
+          case 8: // 바구니
+            playrScore += 2;
+            if(3 <= resources[3].amount && resources[3].amount < 5){
               playrScore += 1;
-            }else if(4 <= resources[i].amount && resources[i].amount <= 5){
+            }else if(5 <= resources[3].amount && resources[3].amount < 7){
               playrScore += 2;
-            }else if(6 <= resources[i].amount && resources[i].amount <= 7){
+            }else if(7 <= resources[3].amount){
               playrScore += 3;
-            }else{
-              playrScore += 4;
             }
-          }else if(resources[i].resourceType === RT.BOAR){
-            if(resources[i].amount === 0){
-              playrScore -= 1;
-            }else if(1 <= resources[i].amount && resources[i].amount <= 2){
-              playrScore += 1;
-            }else if(3 <= resources[i].amount && resources[i].amount <= 4){
-              playrScore += 2;
-            }else if(5 <= resources[i].amount && resources[i].amount <= 6){
-              playrScore += 3;
-            }else{
-              playrScore += 4;
-            }
-          }else if(resources[i].resourceType === RT.CATTLE){
-            if(resources[i].amount === 0){
-              playrScore -= 1;
-            }else if(1 === resources[i].amount){
-              playrScore += 1;
-            }else if(2 <= resources[i].amount && resources[i].amount <= 3){
-              playrScore += 2;
-            }else if(4 <= resources[i].amount && resources[i].amount <= 5){
-              playrScore += 3;
-            }else{
-              playrScore += 4;
-            }
-          }else if(resources[i].resourceType === RT.FARMER){
-            playrScore += resources[i].amount * 3;
-          }else if(resources[i].resourceType === RT.BEGGING){
-            playrScore -= resources[i].amount * 3;
-          }else if(Array.isArray(this.resources[i])){
-            // majorCard 확인, 인덱스 확인해서 점수 계산, 추가점수까지
-            
-          }
+          case 9:
+            playrScore += 4;
+          default:
+            return null;
         }
+      }
+    
 
-        // 빈 칸, 밭, 우리, 방 - tileManager 돌면서 확인, 울타리 쳐진거 판별 어떤식으로?
-
+      // 빈 칸, 밭, 우리, 방 - tileManager 돌면서 확인, 울타리 쳐진거 판별 어떤식으로?
+      
     }
 
     makeTile(tileManager) {
